19e6fd3deee84cda01f4336aef8e15554a90fcff
jorgelucas <jorgelucasl91@gmail.com>
2021-03-03 18:01:19 -0300

    add mais informações na listagem de séries

--- src/components/Series.js
+++ src/components/Series.js
@@ -26,6 +26,8 @@ export function Series() {
             <tr key={record.id}>
                 <th scope="row">{record.id}</th>
                 <td>{record.name}</td>
+                <td>{record.genre.toLowerCase()}</td>
+                <td>{record.status.toLowerCase()}</td>
                 <td>
                     <button className='btn btn-danger' onClick={() => { deleteSerie(record.id) }}>-</button>
                     <Link to={'/series/' + record.id} className='btn btn-info'>Editar</Link>
@@ -51,13 +53,15 @@ export function Series() {
     return (
         <div className='container'>
             <h1>Séries</h1>
-            <Link to='Series/novo' className='btn btn-primary'>Nova Série</Link>
+            <Link to='series/nova' className='btn btn-primary'>Nova Série</Link>
             <hr />
             <table className="table table-striped table-hover">
                 <thead className="thead-dark">
                     <tr>
                         <th scope="col">Id</th>
                         <th scope="col">Nome</th>
+                        <th scope="col">Gênero</th>
+                        <th scope="col">Status</th>
                         <th scope="col">Ações</th>
                     </tr>
                 </thead>
